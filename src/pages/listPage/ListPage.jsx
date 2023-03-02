a40f1bcdf4c14782e7f46abc544b618ec38ed39e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-02 19:55:20 +0100

    ListPageMapfuerFetchVorbereitet

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -5,6 +5,18 @@ import ListItem from "../../components/listItem/ListItem";
 const ListPage = () => {
 
 
+/***************************************************************************************************
+ * 
+ *       Fetch 
+ *      https://pokeapi.co/api/v2/pokemon/{HeaderSearchNames}
+ * 
+ ****************************************************************************************************/
+
+
+
+
+console.log(data);
+
     return (
 
 
@@ -12,12 +24,22 @@ const ListPage = () => {
         <section className="listPage">
 
             {/* // ! .map */}
-            {}
-            <ListItem
-            
-            > </ListItem>
+            {data  &&   data.map((i, index) => {
+
+                return <ListItem
+                    key={index}
+                    img_url={i.img_url}
+                    nr={i.nr}
+                    name={i.name}
+                >
+                </ListItem>
+            })
+            }
         </section>
     );
 }
 
 export default ListPage;
+
+
+
