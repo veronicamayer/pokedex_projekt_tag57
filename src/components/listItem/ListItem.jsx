dc6e72101648b8062bbcd1d65c85e8ea1fe3d509
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-02 16:38:14 +0100

    ListItemFertig

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,1 +1,31 @@
 
+
+
+// import css
+import "./ListItem.scss"
+
+// img import zum testen und stylen
+
+import test_pokemon_image from "../../img/test_pokemon_image 2_.png";
+
+console.log(test_pokemon_image)
+
+
+const ListItem = (props) => {
+    return ( 
+        <section className="listItem">
+
+            <article>
+           {/* {dann img_url} */}
+                <img src={test_pokemon_image} alt={`Bild des Pokemeno {props.name}`} />
+            </article>
+            <article>
+            <p>#001 {props.nr}</p>
+            <p>Bulbasaur {props.name}</p>
+            </article>
+
+        </section>
+     );
+}
+ 
+export default ListItem;
